f136842ba2cc24b99bdd9fc267023fd41271ea72
Sebastien Dornel <kai_dornel@hotmail.com>
2020-03-08 22:41:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -3,29 +3,67 @@ require 'pry'
   
 def load_library(file_path)
   library = YAML.load_file(file_path)
-  # binding.pry
-  
-  meaning_of_emoticons = {"get_meaning" => {}, "get_emoticon" => {} }
-  
+  meaning_of_emoticons = {:get_meaning => {}, :get_emoticon => {}}
   library.each do |meaning_key, emoticon_value|
-    meaning_of_emoticons["get_meaning"][emoticon_value[1]]= meaning_key
-    meaning_of_emoticons["get_emoticon"][emoticon_value[0]] = emoticon_value[1]
+    meaning_of_emoticons[:get_meaning][emoticon_value[1]] = meaning_key
+    meaning_of_emoticons[:get_emoticon][emoticon_value[0]] = emoticon_value[1]
   end
   meaning_of_emoticons
-end
-
+end 
+  
 def get_japanese_emoticon(file_path, emoticon)
-  if load_library(file_path)["get_emoticon"][emoticon]
-    load_library(file_path)["get_emoticon"][emoticon]
+  if load_library(file_path)[:get_emoticon][emoticon]
+    load_library(file_path)[:get_emoticon][emoticon]
   else
     "Sorry, that emoticon was not found"
   end
 end
+
 
 def get_english_meaning(file_path, emoticon)
-  if load_library(file_path)["get_meaning"][emoticon]
-    load_library(file_path)["get_meaning"][emoticon]
+  if load_library(file_path)[:get_meaning][emoticon]
+    load_library(file_path)[:get_meaning][emoticon]
   else
     "Sorry, that emoticon was not found"
   end
 end
+  
+  
+  
+  
+  
+
+  
+  
+  
+  
+  
+  
+# def load_library(file_path)
+#   library = YAML.load_file(file_path)
+#   # binding.pry
+  
+#   meaning_of_emoticons = {"get_meaning" => {}, "get_emoticon" => {} }
+  
+#   library.each do |meaning_key, emoticon_value|
+#     meaning_of_emoticons["get_meaning"][emoticon_value[1]]= meaning_key
+#     meaning_of_emoticons["get_emoticon"][emoticon_value[0]] = emoticon_value[1]
+#   end
+#   meaning_of_emoticons
+# end
+
+# def get_japanese_emoticon(file_path, emoticon)
+#   if load_library(file_path)["get_emoticon"][emoticon]
+#     load_library(file_path)["get_emoticon"][emoticon]
+#   else
+#     "Sorry, that emoticon was not found"
+#   end
+# end
+
+# def get_english_meaning(file_path, emoticon)
+#   if load_library(file_path)["get_meaning"][emoticon]
+#     load_library(file_path)["get_meaning"][emoticon]
+#   else
+#     "Sorry, that emoticon was not found"
+#   end
+# end
